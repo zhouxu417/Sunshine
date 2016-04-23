6c083cf327b693000340c1638a9635996d6a33d4
zhouxu417 <zhouxu417@chy.life>
2016-04-24 01:19:40 +0800

    ShareePreference 保存单位异常，Context无法调用

--- app/src/main/java/com/zhouxu417/xu/sunshine/Sunshine_MainActivity.java
+++ app/src/main/java/com/zhouxu417/xu/sunshine/Sunshine_MainActivity.java
@@ -1,11 +1,15 @@
 package com.zhouxu417.xu.sunshine;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import static android.app.PendingIntent.getActivity;
 
 public class Sunshine_MainActivity extends AppCompatActivity {
 
@@ -20,10 +24,27 @@ public class Sunshine_MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                       // .setAction("Action", null).show();
+            updateWeather();
+
+
             }
         });
+    }
+    //更新天气数据
+    private void updateWeather() {
+
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
+        String location = prefs.getString(getString(R.string.pref_key_location),getString(R.string.pref_default_location));
+        weatherTask.execute(location);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateWeather();
     }
 
 }
